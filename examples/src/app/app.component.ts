d113d2c462b5645c134471a3f03a36103d736382
James Smith <jsmith@tier2consulting.com>
2022-04-04 16:04:04 +0100

    switched examples

--- examples/src/app/app.component.ts
+++ examples/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'examples';
+export class AppComponent implements OnInit {
+  title = 'jrsmiffy';
+  imgUrl = '';
+  githubUrl = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://api.github.com/users/jrsmiffy').subscribe(
+      (repsonse: any) => {
+        console.log(repsonse);
+        this.imgUrl = repsonse.avatar_url;
+        this.githubUrl = repsonse.html_url;
+    });
+  }
+
 }
